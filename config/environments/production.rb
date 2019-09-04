4adbf8fa848c2f49f07863c42b452d538cdfb4cc
RiddhiKcj <ridds509@gmail.com>
2019-09-04 16:42:54 +0530

    production setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+   config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -95,4 +95,16 @@ Rails.application.configure do
   # config.action_mailer.default_url_options = {:host => 'yourdomain.com'}
   # config.action_mailer.delivery_method = :smtp
   Rails.application.routes.default_url_options[:host] = 'https://blognew-rk.herokuapp.com'
+  config.action_mailer.delivery_method = :smtp
+  host = 'https://blognew-rk.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.gmail.com',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => 'ridds509@gmail.com',
+    :password       => 'gudiyap512',
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 end
